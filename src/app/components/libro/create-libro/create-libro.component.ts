ba730ed0ecbe703cbff5f23d315df0c6a0fbda59
JoseOcampoM <jocampo@uniguajira.edu.co>
2022-06-01 20:09:26 -0500

    completo

--- src/app/components/libro/create-libro/create-libro.component.ts
+++ src/app/components/libro/create-libro/create-libro.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { LibroI } from 'src/app/models/libro';
+import { LibroService } from 'src/app/services/libro.service';
 
 @Component({
   selector: 'app-create-libro',
@@ -7,9 +12,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateLibroComponent implements OnInit {
 
-  constructor() { }
+  public formulario: FormGroup = this.formBuilder.group(
+    {
+      titulo: ['', [Validators.required]],
+      numero_pagina: ['', [Validators.required]],
+      editorial: ['', [Validators.required]],
+      isbn: ['', [Validators.required]],
+      AutorId: ['', [Validators.required]],
+    }
+  )
+  constructor(
+    private formBuilder: FormBuilder,
+    private libroService: LibroService,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    const formValue:LibroI = this.formulario.value;
+    this.libroService.createLibro(formValue).subscribe({
+      next:(res: any) => {
+        this.snackBar.open('Libro Creado correctamente', 'OK', {duration:500});
+        this.router.navigateByUrl('/libro');
+      },
+      error:(err: any) => {
+        this.snackBar.open('Error', 'ERROR', {duration:500});
+        console.log(err)
+        this.router.navigateByUrl('/libro');
+      }
+    })
   }
 
 }
